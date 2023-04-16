b4b67990fe6f71e18a7c07620c0cef1cf5cfe006
Lianhao Zheng <128833289+LianhaoZ@users.noreply.github.com>
2023-04-16 13:39:42 -0500

    Title

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ header = """
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale =1, maximum=scale=1, user scalable=no">
-  <title>replit</title>
+  <title>Courses</title>
   <link href="style.css" rel="stylesheet" type="text/css" />
 </head>
 
